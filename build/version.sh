34ebe46f0c8c3cef55cccaec5984d4e9c74ebfc0
Marcel Verkerk <mverkerk@godaddy.com>
2018-02-14 10:53:50 -0700

    Update to deploy scripts fix: add github token to travis file fix: add git commands to version.sh script

--- build/version.sh
+++ build/version.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-version=$(git describe --always --tag --abbrev=0)
+version=$(npm view timings version)
 
 if [ -n "${version}" ]; then
   echo "found version: $version"
   search='("api_version":[[:space:]]*").+(")'
   replace="\1${version/v/}\2"
   sed -i -r "s/${search}/${replace}/g" "package.json"
+  git config user.name "verkurkie"
+  git config user.email "mverkerk@godaddy.com"
   git add .
   git commit -m "Set custom version field to ${version}"
-  git push origin
+  git push --quiet "https://${GH_TOKEN}@${GH_REF}" origin:master > /dev/null 2>&1
 else
   echo "Could not find GIT version ..."
 fi
