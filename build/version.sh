b121b516bc5f02415d5fd0ff55596cdc71081657
Marcel Verkerk <mverkerk@godaddy.com>
2018-02-16 07:37:49 -0700

    Update deploy steps - use semantic-release dry-run for version - run `version.sh` before semantic-release

--- build/version.sh
+++ build/version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-version=$(npm view timings version)
+version=$(./node_modules/.bin/semantic-release --dry-run --branch [secure]_dev | grep "next release version is " | sed -nE 's/(.*?(\bnext\srelease\sversion\sis\s)(.*?))$/\3/p')
 
 if [ -n "${version}" ]; then
   echo "found version: $version"
@@ -12,5 +12,5 @@ if [ -n "${version}" ]; then
   git commit -m "Set custom version field to ${version}"
   git push --quiet "https://${GH_TOKEN}@${GH_REF}" origin:master > /dev/null 2>&1
 else
-  echo "Could not find GIT version ..."
+  echo "Could not extract NEXT version from semantic-release dry-run ..."
 fi
